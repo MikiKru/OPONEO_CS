7a10eabc200b51fa5359d836fd84c0d33f5decf3
MikiKru <michal_kruczkowski@o2.pl>
2020-01-10 10:18:59 +0100

    parzenie kawy v2

--- Program.cs
+++ Program.cs
@@ -8,29 +8,34 @@ namespace ParzenieKawy
 {
     class Program
     {
-        static void Main(string[] args)     // od tej metody uruchamia się projekt
-        {
-            Boolean dodacMleko = false;
-            Boolean dodacCukier = false;
-            int ileCukru = 5;
+        // metoda dostarczająca algorytm parzenia kawy
+        public void coffeeGenerator(Boolean dodacMleko, Boolean dodacCukier, int ileCukru) {
             Console.WriteLine("START");
             Console.WriteLine("Przygotuj składniki");
             Console.WriteLine("Wsyp kawę do filiżanki");
             Console.WriteLine("Zagotuj wodę");
             Console.WriteLine("Zalej kawę gorącą wodą");
             // dodawanie mleka 
-            if (dodacMleko == true) {
+            if (dodacMleko == true)
+            {
                 Console.WriteLine("Dolewam 50ml mleka");
             }
             // dodawanie cukru
-            if (dodacCukier) {
+            if (dodacCukier)
+            {
                 // dodawaj sekwencyjnie cukier -> po jednej łyżeczce
-                for (int i = 1; i <= ileCukru; i++) {
+                for (int i = 1; i <= ileCukru; i++)
+                {
                     Console.WriteLine("Dodaję " + i + " łyżeczkę cukru");
                 }
             }
             Console.WriteLine("Zamieszaj");
             Console.WriteLine("STOP");
+        }
+    
+        static void Main(string[] args)     // od tej metody uruchamia się projekt
+        {
+
         }
     }
 }
